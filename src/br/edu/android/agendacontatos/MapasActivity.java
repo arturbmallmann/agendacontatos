5db5a3f9e9498c88726b02d40777a7be8f9ddc86
artur <artur@biodevel.com>
2014-12-18 22:31:24 -0200

    dsahf

---
+++ src/br/edu/android/agendacontatos/MapasActivity.java
@@ -0,0 +1,19 @@
+package br.edu.android.agendacontatos;
+
+import com.example.agendacontatos.R;
+import com.example.agendacontatos.R.layout;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MapasActivity extends FragmentActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_mapas);
+	}
+}
